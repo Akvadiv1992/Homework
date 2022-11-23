b70d245605a08ce2d7581e8db982927fd988daf6
IGOR <sahmen@mail.ru>
2022-11-24 01:14:59 +0300

    DZ 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+Console.WriteLine($"наибольшее число: {a}");
+if (b > a && b > c)
+Console.WriteLine($"наибольшее число: {b}");
+if (c > a && c > b)
+Console.WriteLine($"наибольшее число: {c}");
