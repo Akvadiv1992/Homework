3e405fe098e5430922b9c608b9f6a304cc743251
IGOR <sahmen@mail.ru>
2022-11-24 01:11:40 +0300

    DZ 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n > m)
+Console.WriteLine($"наибольшее число: {n}");
+if (m > n)
+Console.WriteLine($"наибольшее число: {m}");
+if (n < m)
+Console.WriteLine($"наименьшее число: {n}");
+if (m < n)
+Console.WriteLine($"наименьшее число {m}");
